a7996180a2cc7fbd8953f2556badb2a0fba01372
Romain <r.monier62@hotmail.com>
2025-07-06 00:26:40 +0200

    Fix: update Swagger servers URL

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const blagueRoutes = require('./routes/api/v1/blagues');
 const configurerSwagger = require('./swagger/swagger');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
