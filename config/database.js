cff716a00585dee37a5532fc33e77bcccfe48009
Romain <r.monier62@hotmail.com>
2025-07-06 01:29:51 +0200

    Fix Sequelize explicit dialect for Render

--- config/database.js
+++ config/database.js
@@ -1,14 +1,16 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize({
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  dialect: process.env.DB_DIALECT,
-  logging: false,
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,         // nom de la BDD
+  process.env.DB_USER,         // utilisateur
+  process.env.DB_PASS,         // mot de passe
+  {
+    host: process.env.DB_HOST, // hôte
+    port: process.env.DB_PORT, // port
+    dialect: 'postgres',       // !!! ÉCRIS ICI EN DUR !!!
+    logging: false
+  }
+);
 
 module.exports = sequelize;
